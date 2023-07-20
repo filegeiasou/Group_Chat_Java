8a6dff475f5c8190c6a3d71fa4868038b366b2d6
Aggelos Mentzelos <filegeiasou@gmail.com>
2023-07-20 16:40:59 +0300

    Update Server.java

--- Chat/Server.java
+++ Chat/Server.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package javaapplication10;
 
 import java.net.*;
 import java.io.*;
